0fb49064f902c9158c18bdea850858fbf091724d
Vikneshwaren <vikneshwaren.u@gmail.com>
2014-05-31 11:55:03 +0530

    Update AnnaUnivBot.py

--- AnnaUnivBot.py
+++ AnnaUnivBot.py
@@ -9,6 +9,7 @@ RNO = raw_input("Register No: ")
 DOB = raw_input("Date of Birth: ")
 
 # Login to site
+print "Hitting the site."
 url = 'http://coe1.annauniv.edu/home/index.php'
 br = mechanize.Browser()
 br.set_handle_equiv(True)
@@ -20,6 +21,7 @@ br.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6;
 br.open(url)
 
 # Captcha
+print "Resolving Captcha.."
 soup = BeautifulSoup(br.response().read())
 stripper_captcha = soup.find_all('form')[1].contents[3].text[74:95].encode('utf-8')
 pat = "\((.*?)\)"
@@ -39,19 +41,23 @@ if a[0].find('-') != -1:
         a2 = a[0][sympos+1:len_str]
         captcha = int(a1)-int(a2)
         captcha = str(captcha) #We got the answer for the captch here in 'captcha'
-
+print "Captcha Resolved..."
 
 
 # Submitting students login form
+print "Filling Students Login form...."
 br.select_form(name = "login_stu")
 br["register_no"] = RNO
 br["dob"] = DOB
 br["security_code_student"] = captcha
 
 # After successful submission, We got into the students profile portal now
+print "Submitting the form to the server....."
 response = br.submit()
 
 # We are navigating to Results tab
+print "Entered into Students portal......"
+print "Navigating to 'Exam Results' tab......."
 br.select_form(name = "formExamResults")
 response = br.submit()
 
